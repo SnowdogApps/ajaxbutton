bf35a00ffcdd68a27f2d337cfd7da0b2fb20829a
maciekkossuda <maciek@kossuda.pl>
2013-07-01 09:55:38 +0200

    little changes

--- code/Model/Logic.php
+++ code/Model/Logic.php
@@ -11,7 +11,7 @@ class Snowdog_AjaxButton_Model_Logic {
     switch ($price) {
       case $price < 50:
         $result = 'cheap'; break;
-      case $price < 100;
+      case $price < 200;
         $result = 'ok'; break;
       case $price < 500;
         $result = 'expensive'; break;
